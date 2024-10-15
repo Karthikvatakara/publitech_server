826625f18373fc95781c0d96a4205b738f68c03e
karthik <krishnankarthik07@gmail.com>
2024-10-15 19:22:44 +0530

    fix: notification route changed

--- notification-service/src/infrastructure/services/sendForgotPasswordMail.ts
+++ notification-service/src/infrastructure/services/sendForgotPasswordMail.ts
@@ -2,7 +2,7 @@ import { forgotPasswordMail } from "../../_lib/nodemailer";
 
 export const sendForgotPasswordMail = async( email: string,token: string) => {
     try{
-        await forgotPasswordMail({ email: email,url: `http://localhost:5173/auth/change-password?token=${token}`})
+        await forgotPasswordMail({ email: email,url: `https://publitech-client.vercel.app/auth/change-password?token=${token}`})
     }catch(error:any){
         throw new Error(error?.message);
     }
